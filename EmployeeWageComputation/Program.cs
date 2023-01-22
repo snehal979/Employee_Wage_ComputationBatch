e08da2e16cbd9ffc0a96ac70f1d06804de13771a
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 00:16:53 +0530

    [Snehal]Add: Calculate Employee Part time or Full time Daily Wage using Switch Case

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,26 +12,27 @@ namespace EmployeeWageComputation
 
             //Uc2 Calculate Daily Wage
             //Uc3 Calculate Daily Wage for Part time
+            //Uc4 Using Switch case
             int empAttaindance = random.Next(3);//0 or 1 or 2 print
             //variable
             string type = "";
             int Per_Daily_Wage = 20;
             int Daily_Hour = 0;
-            if (empAttaindance == 0)
+            switch(empAttaindance)
             {
+                case 0:
                 type ="Absent";
                 Daily_Hour = 0;
-            }
-            else if (empAttaindance == 1)
-            {
-                type ="PartTimeJob";
-                Daily_Hour = 4;
-            }
-            else
-            {
-                type ="FullTimeJob";
-                Daily_Hour = 8;
-            }
+                    break;
+                case 1:
+                    type ="PartTimeJob";
+                    Daily_Hour = 4;
+                    break;
+                default :
+                    type ="FullTimeJob";
+                    Daily_Hour = 8;
+                    break;
+            }  
             int dailyWage = Daily_Hour * Per_Daily_Wage;
             Console.WriteLine("Employee is {0}  and Daily Wage of emp is {1}", type, dailyWage);
             Console.ReadLine();
