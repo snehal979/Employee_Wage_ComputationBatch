30b23d90444ce769084620d595798636f6a0fdb6
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 00:08:34 +0530

    [Snehal]Add: Calculate Employee Daily Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,21 +1,32 @@
-﻿namespace EmployeeWageComputation
+﻿using System;
+
+namespace EmployeeWageComputation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME THE EMPLOYEE WAGE PROGRAM");
+            //Uc1 
             Random random = new Random(); //Random method
             int empAttaindance = random.Next(2);//0 or 1 print
-
-            if (empAttaindance == 0) // if condition is true then excu. statement
+            //Uc2 Calculate Daily Wage
+            //variable
+            string type = "";
+            int Per_Daily_Wage = 20;
+            int Daily_Hour = 0;
+            if (empAttaindance == 0)
             {
-                Console.WriteLine("Employee is Absent");
+                type ="Absent";
+                Daily_Hour = 0;
             }
             else
             {
-                Console.WriteLine("Employee is Present");
+                type ="Present";
+                Daily_Hour = 8;
             }
+            int dailyWage = Daily_Hour * Per_Daily_Wage;
+            Console.WriteLine("Employee is {0}  and Daily Wage of emp is {1}", type, dailyWage);
             Console.ReadLine();
         }
 
