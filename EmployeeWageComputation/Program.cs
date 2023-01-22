86c76a50ba3df600fc04a66cac1814281036f585
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 00:29:54 +0530

    [Snehal]Add: Calculate Employee Part time or Full time MONTH Wage using Switch Case

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -13,31 +13,37 @@ namespace EmployeeWageComputation
             //Uc2 Calculate Daily Wage
             //Uc3 Calculate Daily Wage for Part time
             //Uc4 Using Switch case
-            int empAttaindance = random.Next(3);//0 or 1 or 2 print
+            //Uc 5 Calculate Wage for month (for loop ) due to using final condition
             //variable
             string type = "";
-            int Per_Daily_Wage = 20;
+            int Rate_Per_Daily_Wage = 20;
             int Daily_Hour = 0;
-            switch(empAttaindance)
+            int Working_Day = 20;
+            int totaldailyWage = 0;
+            for (int i = 1; i<=Working_Day; i++)
             {
-                case 0:
-                type ="Absent";
-                Daily_Hour = 0;
-                    break;
-                case 1:
-                    type ="PartTimeJob";
-                    Daily_Hour = 4;
-                    break;
-                default :
-                    type ="FullTimeJob";
-                    Daily_Hour = 8;
-                    break;
-            }  
-            int dailyWage = Daily_Hour * Per_Daily_Wage;
-            Console.WriteLine("Employee is {0}  and Daily Wage of emp is {1}", type, dailyWage);
-            Console.ReadLine();
+                int empAttaindance = random.Next(3);//0 or 1 or 2 print
+                switch (empAttaindance)
+                {
+                    case 0:
+                        type ="Absent";
+                        Daily_Hour = 0;
+                        break;
+                    case 1:
+                        type ="PartTimeJob";
+                        Daily_Hour = 4;
+                        break;
+                    default:
+                        type ="FullTimeJob";
+                        Daily_Hour = 8;
+                        break;
+                }
+                totaldailyWage = totaldailyWage + Daily_Hour; //4=0+4
+                Console.WriteLine("employee is {0}  and total hour working day is {1}", type, totaldailyWage);                                                  //12=4+8
+            }
+            int Total_Month_Wage = totaldailyWage * Rate_Per_Daily_Wage;
+            Console.WriteLine("total month salary {0}", Total_Month_Wage);
+            Console.ReadLine();   
         }
-
-
     }
 }
