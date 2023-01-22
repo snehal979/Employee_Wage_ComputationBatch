3cdf6f31c1fc9adfb5ba80ca4f72a304e0ac77ba
Snehal979 <bansodsnehal535@gmail.com>
2023-01-22 23:59:36 +0530

    [SnehalAdd: Check the employee is present or absent

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,7 +5,20 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME THE EMPLOYEE WAGE PROGRAM");
+            Random random = new Random(); //Random method
+            int empAttaindance = random.Next(2);//0 or 1 print
+
+            if (empAttaindance == 0) // if condition is true then excu. statement
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
             Console.ReadLine();
         }
+
+
     }
 }
